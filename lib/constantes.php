80475e9a7d385fae4d353e80816b5102cee56c72
cetecom <cetecom@LCV20406>
2019-03-15 15:54:35 -0300

    nuevo project

---
+++ lib/constantes.php
@@ -0,0 +1,6 @@
+<?php
+
+define('URLBASE','http://localhost:801/hojas/');
+
+//definida constante que es la url de coneccion
+//
---
+++ menu.php
@@ -0,0 +1,27 @@
+<!--<div class="list-group">
+<a href="<?=URLBASE;?>"><i class="fa fa-home fa-2x" class="list-group-item"></i> Home</a>
+<br>
+<a href="#" class="list-group-item"><i class="fa fa-file-o fa-2x" class="list-group-item"></i> Solicitudes</a>
+<br>
+<a class="submenu" href="<?=URLBASE;?>contenido/vacaciones.php" class="list-group-item"><i class="fa fa-sun-o fa-2x" class="list-group-item"></i> Vacaciones</a>
+<br>
+<a href="#"><i class="fa fa-birthday-cake fa-2x" class="list-group-item"></i> Cumpleaños</a>
+<br>
+<a href="#"><i class="fa fa-file-o fa-2x" class="list-group-item"></i> Certificados</a>
+<br>
+<a href="#"><i class="fa fa-file-o fa-2x" class="list-group-item"></i> Resumen</a>
+</div>-->
+
+<div class="list-group">
+  <a class="list-group-item" href="<?=URLBASE;?>"><i class="fa fa-home fa-fw" aria-hidden="true"></i>&nbsp; Home</a>
+  <br>
+  <a href="#" class="list-group-item"><i class="fa fa-file-o fa-2x" class="list-group-item"></i> Solicitudes</a>
+  <br>
+  <a class="submenu" class="list-group-item" href="<?=URLBASE;?>contenido/vacaciones.php"><i class="fa fa-book fa-fw" aria-hidden="true"></i>&nbsp; Vacaciones</a>
+  <br>
+  <a class="list-group-item" href="#"><i class="fa fa-pencil fa-fw" aria-hidden="true"></i>&nbsp; Cumpleaños</a>
+  <br>
+  <a class="list-group-item" href="#"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Certificados</a>
+  <br>
+  <a class="list-group-item" href="#"><i class="fa fa-cog fa-fw" aria-hidden="true"></i>&nbsp; Resumen</a>
+</div>
